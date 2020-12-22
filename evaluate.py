dc7148d0020060d3541e975d05e611af375de5b3
Kirsan <saito-@kik@hotmail.com>
2020-12-22 11:30:16 +0100

    TODO : ajouter un opening book pour les 5-10 premiers coups

--- evaluate.py
+++ evaluate.py
@@ -1,25 +1,59 @@
 import Goban
 import evaluateCluster
+import numpy as np
 ########################
 # Fonction evaluation (heuristique): 
 ########################
-def evaluate(self):
-    print("Voissins: ",self._neighborsEntries)
-    score_black = self._nbBLACK
-    score_white = self._nbWHITE
+
+def evaluate(b):
+    score_black = b._nbBLACK
+    score_white = b._nbWHITE
     scoreTotal = score_white - score_black
     return scoreTotal
 
-def evaluate(b):
-    print('eval')
-    color=b._BLACK
-    a=1
-    c=1
-    score = 0 
-    for stringNumber in b._stringUnionFind:
-        stringSizes=b._stringSizes[stringNumber]
-        stringLiberties=b._stringLiberties[stringNumber]
-        if b[stringNumber] == color:     #b.__getitem__(b,stringNumber) == color:     
-            score+= a * stringSizes**2 + c * stringLiberties
-        else :   
-            score-= a * stringSizes**2 + c * stringLiberties
+# Evaluate diff 
+# def evaluate(b):
+#     black_stones = 0
+#     white_stones = 0
+#     for r in range(1, b._BOARDSIZE + 1):
+#         for c in range(1, b._BOARDSIZE + 1):
+#             p = b._BOARDSIZE * r+c
+#             color = b._WHITE
+#             if color == b._BLACK:
+#                 black_stones += 1
+#             elif color == b._WHITE:
+#                 white_stones += 1
+#     diff = black_stones - white_stones
+#     if b.next_player == b._BLACK:
+#         return diff
+#     return -1 * diff
+
+
+# Evaluate Cluster 
+# def evaluate(b):
+#     b._neighbors = []
+#     b._neighborsEntries = []
+#     for nl in [b._get_neighbors(fcoord) for fcoord in range(b._BOARDSIZE**2)] :
+#         b._neighborsEntries.append(len(b._neighbors))
+#         for n in nl:
+#             b._neighbors.append(n)
+#             print(b._neighbors)
+#         b._neighbors.append(-1) # Sentinelle
+#     b._neighborsEntries = np.array(b._neighborsEntries, dtype='int16')
+#     b._neighbors = np.array(b._neighbors, dtype='int8')
+#     print(b._neighborsEntries)
+#     return b._neighbors
+
+# def evaluate(b):
+#     print('eval')
+#     color=b._BLACK
+#     a=1
+#     c=1
+#     score = 0 
+#     for stringNumber in b._stringUnionFind:
+#         stringSizes=b._stringSizes[stringNumber]
+#         stringLiberties=b._stringLiberties[stringNumber]
+#         if b[stringNumber] == color:     #b.__getitem__(b,stringNumber) == color:     
+#             score+= a * stringSizes**2 + c * stringLiberties
+#         else :   
+#             score-= a * stringSizes**2 + c * stringLiberties
--- myPlayer.py
+++ myPlayer.py
@@ -31,7 +31,7 @@ class myPlayer(PlayerInterface):
             print("Referee told me to play but the game is over!")
             return "PASS"     
         moves = self._board.legal_moves() # Dont use weak_legal_moves() here!
-        move = IAAlphaBeta(self._board, depth=2)
+        move = IDAlphaBeta(self._board, depth=3)
         self._board.push(move)
 
         # New here: allows to consider internal representations of moves
