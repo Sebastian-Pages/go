65d7e4a702d461a0347e185fb870eb5953e0b219
Sebastian-Pages <yves-sebastian.pages@telecom-sudparis.eu>
2020-12-22 12:42:18 +0100

    first opening strategy

---
+++ opening.py
@@ -0,0 +1,13 @@
+import Goban
+
+bestMoves= [(5,5)]
+
+def Opening(b, count,limiteCPU=None):
+    coup = getBestMove(b)
+    return coup
+
+def getBestMove(b) :
+    for i in bestMoves:
+        if i in b.generate_legal_moves() :
+            bestMoves.pop
+        return i
